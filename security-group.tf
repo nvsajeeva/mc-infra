0d099b5244d121fa1dc8e9240daa5af3c62ed765
slakmal <slakmal@wiley.com>
2022-03-23 00:30:28 +0530

    Change sg

--- security-group.tf
+++ security-group.tf
@@ -40,6 +40,14 @@ resource "aws_security_group" "mc-ghost-sg" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+  ingress {
+    description      = "traffic from outside"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0

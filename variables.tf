7c526e83016ebfce9129300b652305a280271439
slakmal <slakmal@wiley.com>
2022-03-22 11:09:23 +0530

    Add rds resource

--- variables.tf
+++ variables.tf
@@ -6,3 +6,22 @@ variable "keypair_name" {
   description = "KeyPair Name"
   default     = "mc-keypair"
 }
+variable "mysql_engine_version" {
+  type        = string
+  default     = "8.0"
+}
+
+variable "mysql_instance_class" {
+  type    = string
+  default = "db.t3.micro"
+}
+
+variable "instance_name" {
+  type    = string
+  default = "ghostdb"
+}
+
+variable "mysql_parameter_group_name" {
+  type    = string
+  default = "default.mysql8.0"
+}
--- vpc.tf
+++ vpc.tf
@@ -8,7 +8,8 @@
 
 resource "aws_vpc" "demo" {
   cidr_block = "10.0.0.0/16"
-
+  enable_dns_hostnames = true
+  enable_dns_support = true
   tags = tomap({
     "Name"                                      = "mc-vpc"
   })
